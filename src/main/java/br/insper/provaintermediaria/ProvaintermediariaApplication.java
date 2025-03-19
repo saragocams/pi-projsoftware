ba176fcbb2758fa5876a4a5f2ba41a12d0b467c4
saragocams <saragocams@gmail.com>
2025-03-19 10:30:48 -0300

    commit 1

---
+++ src/main/java/br/insper/provaintermediaria/ProvaintermediariaApplication.java
@@ -0,0 +1,13 @@
+package br.insper.provaintermediaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaintermediariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaintermediariaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/insper/provaintermediaria/teste/TesteController.java
@@ -0,0 +1,15 @@
+package br.insper.provaintermediaria.teste;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "Hello World";
+    }
+
+}
